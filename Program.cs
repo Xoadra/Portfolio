831fddc0b383faee76c152ee948bfbaea594594e
Xoadra <xijp77@gmail.com>
2018-01-31 23:43:03 -0800

    Configuration files added back to public repo.

--- Program.cs
+++ Program.cs
@@ -2,30 +2,24 @@
 
 
 
-//using System;
-//using System.Collections.Generic;
 using System.IO;
-//using System.Linq;
-//using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-//using Microsoft.Extensions.Configuration;
-//using Microsoft.Extensions.Logging;
 
 
 namespace Xambda {
     public class Program {
 		
-        public static void Main( string[ ] args ) { BuildWebHost( args ).Run( ); }
+    	public static void Main( string[ ] args ) { BuildWebHost( args ).Run( ); }
 		
         public static IWebHost BuildWebHost( string[ ] args ) =>
             WebHost.CreateDefaultBuilder( args )
 				.UseWebRoot( "Root" )
+				.UseUrls( "http://localhost:7700" )
                 .UseStartup<Startup>( )
                 .Build( );
 		
     }
 }
-
 
 
--- Startup.cs
+++ Startup.cs
@@ -2,17 +2,10 @@
 
 
 
-using System;
-using System.IO;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-//using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
 
 
 namespace Xambda {
@@ -49,7 +42,6 @@ namespace Xambda {
 			soft.UseStaticFiles( );
 			soft.UseSession( );
 			soft.UseMvc( );
-			
 		}
 	
 	}
